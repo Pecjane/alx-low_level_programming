d06c3b13d208b6a67d55f4caea9e68da9034a2ef
Pecjane <promisebarth@gmail.com>
2023-08-13 07:21:45 -0700

    hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	printf("Size of char: %lu bytes\n", sizeof(char));
 	printf("Size of a long: %lu bytes\n", sizeof(long));
 	printf("Size of long long: %lu bytes\n", sizeof(long long));
-	printf("Size of float: %lu bytes\n", sizeof(float)	
+	printf("Size of float: %lu bytes\n", sizeof(float));
 	return (0);
 }
-
