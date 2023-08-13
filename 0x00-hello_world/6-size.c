fedb0dc14eaac3d18a3c05c1c24191707d3675c2
Pecjane <promisebarth@gmail.com>
2023-08-13 07:36:55 -0700

    size of gradeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	printf("Size of char: %lu bytes\n", sizeof(char));
-	printf("Size of char: %lu bytes\n", sizeof(char));
+	printf("Size of int: %lu bytes\n", sizeof(int));
 	printf("Size of a long: %lu bytes\n", sizeof(long));
 	printf("Size of long long: %lu bytes\n", sizeof(long long));
 	printf("Size of float: %lu bytes\n", sizeof(float));
